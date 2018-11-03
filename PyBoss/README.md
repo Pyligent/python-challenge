e909bf20aa964f11ef531cae865a897f3d3c1d45
Tao Jin (Stephen) <jin.hybr@gmail.com>
2018-11-02 20:31:13 -0400

    Add files via upload

---
+++ PyBoss/README.md
@@ -0,0 +1,37 @@
+# PyBoss
+
+![Boss](Images/boss.jpg)
+
+In this challenge, you get to be the _boss_. You oversee hundreds of employees across the country developing Tuna 2.0, a world-changing snack food based on canned tuna fish. Alas, being the boss isn't all fun, games, and self-adulation. The company recently decided to purchase a new HR system, and unfortunately for you, the new system requires employee records be stored completely differently.
+
+Your task is to help bridge the gap by creating a Python script able to convert your employee records to the required format. Your script will need to do the following:
+
+* Import the `employee_data1.csv` and `employee_data2.csv` files, which currently holds employee records like the below:
+
+```csv
+Emp ID,Name,DOB,SSN,State
+214,Sarah Simpson,1985-12-04,282-01-8166,Florida
+15,Samantha Lara,1993-09-08,848-80-7526,Colorado
+411,Stacy Charles,1957-12-20,658-75-8526,Pennsylvania
+```
+
+* Then convert and export the data to use the following format instead:
+
+```csv
+Emp ID,First Name,Last Name,DOB,SSN,State
+214,Sarah,Simpson,12/04/1985,***-**-8166,FL
+15,Samantha,Lara,09/08/1993,***-**-7526,CO
+411,Stacy,Charles,12/20/1957,***-**-8526,PA
+```
+
+* In summary, the required conversions are as follows:
+
+  * The `Name` column should be split into separate `First Name` and `Last Name` columns.
+
+  * The `DOB` data should be re-written into `MM/DD/YYYY` format.
+
+  * The `SSN` data should be re-written such that the first five numbers are hidden from view.
+
+  * The `State` data should be re-written as simple two-letter abbreviations.
+
+* Special Hint: You may find this link to be helpful—[Python Dictionary for State Abbreviations](https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5).
---
+++ PyBoss/pyboss_tj.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 16:30:55 2018
+
+@author: my_macbook
+"""
+
+# Path to collect data from the Resources folder
+import csv
+from datetime import datetime
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+# Path to collect data from the Resources folder
+csvpath = 'employee_data.csv'
+
+
+# Read in the CSV file
+with open(csvpath, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+    employee_data = []
+    
+    for row in csvreader:
+        employee_data.append(row)
+        
+
+for i in range(len(employee_data)):
+    #Split the name:
+    Firstname_Lastname = employee_data[i][1].split(' ')
+    #Format the DOB:
+    DOB_date = datetime.strptime(employee_data[i][2],'%Y-%m-%d')
+    DOB_date_MMDDYY = datetime.strftime(DOB_date,'%m/%d/%Y')
+    #Format the SSN
+    SSN_lastfour = employee_data[i][3][6:]
+    Hidden_SSN = '***-**' + SSN_lastfour
+    # Format the State:
+    State_Abbr = us_state_abbrev.get(employee_data[i][4])
+    #Build new data list
+    employee_data[i] = employee_data[i][:1] 
+    employee_data[i].append(Firstname_Lastname[0])
+    employee_data[i].append(Firstname_Lastname[1])
+    employee_data[i].append(DOB_date_MMDDYY)
+    employee_data[i].append(Hidden_SSN)
+    employee_data[i].append(State_Abbr)
+
+
+# Output new date file
+
+with open('employee_file.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer.writerow(['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State'])
+
+    for newdata in employee_data:
+        employee_writer.writerow(newdata)  
+
